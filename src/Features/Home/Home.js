ae2830dfd83ea6d08d99b17e67ae0509f98f57ff
Hanna Tiara Andarlia <hannatiara@gmail.com>
2021-01-22 03:43:24 +0700

    Connect store to HomePage with some movie items

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,14 +1,94 @@
-import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {FlatList} from 'react-native-gesture-handler';
 
-export default function Home(props) {
-  const actionDetails = () => {
-    props.navigation.navigate('MovieDetails');
+// component
+import TrendingItem from '../../Shared/Components/TrendingItem.';
+import NowPlayingItem from '../../Shared/Components/NowPlayingItem';
+import Roboto from '../../Shared/Components/Roboto';
+
+// config
+import {Layouting} from '../../Shared/Global/Style/Layout';
+import {Size} from '../../Shared/Global/Config/Size';
+import {Color} from '../../Shared/Global/Config/Color';
+
+// redux
+import {connect} from 'react-redux';
+
+// actions
+import {actionTrending} from '../../Store/Actions/actionTrending';
+import {actionNowPlaying} from '../../Store/Actions/actionNowPlaying';
+
+function Home(props) {
+  useEffect(() => {
+    props.actionNowPlaying();
+    props.actionTrending();
+  }, []);
+
+  const screenDetailPage = (id) => {
+    props.navigation.navigate('MovieDetails', id);
   };
 
   return (
-    <TouchableOpacity onPress={actionDetails}>
-      <Text>Home</Text>
-    </TouchableOpacity>
+    <SafeAreaView style={[Layouting().flex1, Size.wp92]}>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={props.trending}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => {
+          return (
+            <TrendingItem
+              movie={item}
+              onPress={() => screenDetailPage(item.id)}
+              opacity={1}
+            />
+          );
+        }}
+      />
+
+      <View
+        style={[
+          Layouting().centerAlign,
+          {marginTop: Size.ms28, marginBottom: Size.ms12},
+        ]}>
+        <Roboto
+          title="NOW PLAYING"
+          color={Color.red}
+          size={Size.ms28}
+          type="Black"
+        />
+      </View>
+
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={props.nowPlaying}
+        numColumns={2}
+        renderItem={({item}) => {
+          return (
+            <NowPlayingItem
+              movie={item}
+              onPress={() => screenDetailPage(item.id)}
+            />
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    trending: state.TrendingReducer.trendingMovies,
+    nowPlaying: state.NowPlayingReducer.nowPlayingMovies,
+  };
+};
+
+const mapDispatchToProps = {
+  actionTrending,
+  actionNowPlaying,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
