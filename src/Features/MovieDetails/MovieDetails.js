df73679b21498e39665e7992ef4a16f26df113d9
Hanna Tiara Andarlia <hannatiara@gmail.com>
2021-01-22 03:44:49 +0700

    Connect store and Home Page to Movie Details Page

--- src/Features/MovieDetails/MovieDetails.js
+++ src/Features/MovieDetails/MovieDetails.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+// redux
+import {connect} from 'react-redux';
 
-export default function MovieDetails() {
+// action
+import {actionDetails} from '../../Store/Actions/actionDetails';
+
+function MovieDetails(props) {
+  useEffect(() => {
+    props.actionDetails(props.route.params);
+  }, []);
+
   return (
     <View>
-      <Text>Details</Text>
+      <Text>{props.details.original_title}</Text>
     </View>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    details: state.DetailsReducer.itemDetails,
+  };
+};
+
+const mapDispatchToProps = {
+  actionDetails,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetails);
