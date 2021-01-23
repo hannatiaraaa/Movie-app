9976047c23985cdabb27f581ffcbdef943bff3fd
Hanna Tiara Andarlia <hannatiara@gmail.com>
2021-01-24 00:40:22 +0700

    Change Header details to navigation

--- src/Router/Navigation.js
+++ src/Router/Navigation.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+
+// screen
 import Home from '../Features/Home/Home';
 import MovieDetails from '../Features/MovieDetails/MovieDetails';
 
 const Stack = createStackNavigator();
 export default function Navigation() {
   return (
-    <Stack.Navigator initialRouteName={Home}>      
+    <Stack.Navigator initialRouteName={Home}>
       <Stack.Screen
         name="Home"
         component={Home}
         options={{headerShown: false}}
       />
-      <Stack.Screen name="MovieDetails" component={MovieDetails} options={{headerShown: false}}/>
+      <Stack.Screen
+        name="MovieDetails"
+        component={MovieDetails}
+        options={{headerTransparent: 'true', title: ''}}
+      />
     </Stack.Navigator>
   );
 }
